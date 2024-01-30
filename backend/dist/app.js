52688ad51e2ecc4dee9ce852db52586b871ee2f6
sairamya31 <lankasairamya3103@gmail.com>
2024-01-30 17:06:52 +0530

    Updated Backend

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -1,8 +1,14 @@
 import express from 'express';
 import { config } from "dotenv";
+import morgan from 'morgan';
+import appRouter from './routes/index.js';
+import cookieParser from 'cookie-parser';
 config();
 const app = express();
 //we will be using json format for incoming and outgoing data
 app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(morgan("env"));
+app.use("/api/v1", appRouter);
 export default app;
 //# sourceMappingURL=app.js.map
